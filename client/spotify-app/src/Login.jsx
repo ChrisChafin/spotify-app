1f78dd83da3e97b47df87c040c845276c6ba7798
ChrisChafin <chrischafin11@gmail.com>
2023-02-14 14:50:27 -0600

    URL fix

--- client/spotify-app/src/Login.jsx
+++ client/spotify-app/src/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LoginButton, LoginLink } from "./styles/Login.styles"
 
 const Login = () => {
-    const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.ea5965a4a1354c19b04d0df96c66f139}&response_type=code&redirect_uri=${process.env.localhost:3000}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+    const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.ea5965a4a1354c19b04d0df96c66f139}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
     //try this if above is not working
     //const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
